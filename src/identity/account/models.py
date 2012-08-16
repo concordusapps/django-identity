4aee08079f7a04bc1febc0629c38037a64dc899c
Ryan Leckey <leckey.ryan@gmail.com>
2012-08-15 17:11:19 -0700

    Removed unused fields from profile (for now).

--- src/identity/account/models.py
+++ src/identity/account/models.py
@@ -20,29 +20,10 @@ class Profile(models.Model):
     user = models.OneToOneField(User, editable=False)
 
     ## TODO
-    formatted = models.CharField(max_length=2048, null=True, blank=True)
-
-    ## TODO
     given_name = models.CharField(max_length=512, null=True, blank=True)
 
     ## TODO
     family_name = models.CharField(max_length=512, null=True, blank=True)
-
-    ## TODO
-    middle_name = models.CharField(max_length=512, null=True, blank=True)
-
-    ## TODO
-    salutation = models.CharField(max_length=128, null=True, blank=True)
-
-    ## TODO
-    generation_qualifier = models.CharField(
-        max_length=128,
-        null=True,
-        blank=True
-    )
-
-    ## TODO
-    title = models.CharField(max_length=128, null=True, blank=True)
 
     def __unicode__(self):
         """Returns a textual representation of this."""
