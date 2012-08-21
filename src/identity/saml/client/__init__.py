5cd83b08469b0a3a309750441be263f05e2a0a49
Ryan Leckey <leckey.ryan@gmail.com>
2012-08-20 23:33:34 -0700

    Declared client package.

---
+++ src/identity/saml/client/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+""" \file identity/saml/views/provider.py
+\brief Implements the SAML endpoints for providers.
+
+\author Erich Healy (cactuscommander) ErichRHealy@gmail.com
+\author Ryan Leckey (mehcode) leckey.ryan@gmail.com
+
+\copyright Copyright 2012 © Concordus Applications, Inc.
+           All Rights Reserved.
+"""
